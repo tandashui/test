dc0b323f6d344bc2633f3b79f2e8b960f8e29088
tandashui <tandashui8686@163.com>
2016-10-17 23:31:50 +0800

    ceshi

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,6 +3,24 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        $this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px } a,a:hover{color:blue;}</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p><br/>版本 V{$Think.version}</div><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_55e75dfae343f5a1"></thinkad><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
+        //获得参数 signature nonce token timestamp echostr
+		$nonce     = $_GET['nonce'];
+		$token     = 'imooc';
+		$timestamp = $_GET['timestamp'];
+		$echostr   = $_GET['echostr'];
+		$signature = $_GET['signature'];
+		//形成数组，然后按字典序排序
+		$array = array();
+		$array = array($nonce, $timestamp, $token);
+		sort($array);
+		//拼接成字符串,sha1加密 ，然后与signature进行校验
+		$str = sha1( implode( $array ) );
+		if( $str  == $signature && $echostr ){
+			//第一次接入weixin api接口的时候
+			echo  $echostr;
+			exit;
+		}else{
+			$this->reponseMsg();
+		}
     }
 }
