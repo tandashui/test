2b9ee52d709d03e85dc6ea761421af27583a63ef
tandashui <tandashui8686@163.com>
2016-10-18 14:11:33 +0800

    ceshi

--- index.php
+++ index.php
@@ -13,6 +13,8 @@
 
 // 检测PHP环境
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
+//报告所有错误
+error_reporting(E_ALL);
 
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG',True);
