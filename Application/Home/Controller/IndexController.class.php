f543fe5615ebc7e81a1756278c7774805dae33d7
tandashui <tandashui8686@163.com>
2016-10-21 01:10:37 +0800

    ceshi

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -441,7 +441,7 @@ public function sendTemplate(){
 
 	//拼接数组
 	$array = array(
-			'touser'=>'oDPp3wGPDKgxAxOD4abq-x9stm-s',
+			'touser'=>'oDPp3wBe1bTmlRByvg_aCBOWrjYI',
 			'template_id'=>'mUOkO0oRvQHmEV21lG9VPorR1pnYPspV47_UDVeiQEc',
 			'url'=>'http://www.tandashui.com',
 			'data'=>array(
